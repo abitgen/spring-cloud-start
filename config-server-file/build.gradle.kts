1ad93b80e72c1284c5fa02f23baa196e2e1472ca
abitgen <abitgenius@gmail.com>
2020-08-17 21:48:34 +0530

    Config server get from filesystem
    closes #4

---
+++ config-server-file/build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+
+}
+
+buildscript {
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.3.1.RELEASE")
+    }
+}
+
+
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "Hoxton.SR6"
+
+dependencies {
+    //implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    //implementation("org.springframework.boot:spring-boot-bus-amqp")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.bootRun{
+    // support passing -Dsystem.property=value to bootRun task
+    //systemProperties = System.getProperties()
+    //systemProperties.set("MY_GIT_PASSWORD","123")
+    if (project.hasProperty("MY_GIT_PASSWORD")){
+
+    }
+    //println(System.getProperties())
+}
---
+++ config-server-file/src/main/java/io/github/abitgen/configserver/ConfigServerApplication.kt
@@ -0,0 +1,19 @@
+package io.github.abitgen.configserver
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.core.env.Environment
+
+@SpringBootApplication
+@EnableConfigServer
+open class ConfigServerApplication @Autowired constructor(val env: Environment) : CommandLineRunner{
+    override fun run(vararg args: String?) {
+
+    }
+}
+fun main(args: Array<String>) {
+    SpringApplication.run(ConfigServerApplication::class.java, *args)
+}
